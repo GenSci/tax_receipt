14ba13da214ee0060dcfbb59cfc2af0ffc860b53
Ryan <ryan.manzer@gmail.com>
2018-09-16 10:03:38 -0700

    Built out basic donut chart

--- static/index.js
+++ static/index.js
@@ -11,7 +11,7 @@ function drawGraphPie(taxes_paid=10489) {
           width = +svg.attr('width'),
           height = +svg.attr('height'),
           radius = Math.min(width, height) / 2,
-          g = svg.append('g')..attr('transform', 'translate(' + width/2 + ',' + height/2 + ')');
+          g = svg.append('g').attr('transform', 'translate(' + width/2 + ',' + height/2 + ')');
 
           var pie = d3.pie().sort(null)
                             .value(function(d){ return d.funds});
@@ -24,25 +24,35 @@ function drawGraphPie(taxes_paid=10489) {
                             .outerRadius(radius)
                             .innerRadius(radius);
 
-        d3.json('static/data.json', function(d){
-            d.percentage = +d.percentage;
-            return d;
-        }, function(error, data){
+        d3.json('static/data.json', function(error, data){
             if (error) {
                 throw error;
             }
+            var pie_data = new Array
+            var pnum = 1/Object.keys(data).length;
+            var step = 0;
+            for (var comp in data) {
+                if (data.hasOwnProperty(comp)) {
+                    let comp_obj = {
+                        name: comp,
+                        funds: +data[comp].percentage * taxes_paid,
+                        color: d3.interpolateRdBu(step)
+                    };
+                    pie_data.push(comp_obj);
+                    step += pnum;
+                }
+            }
             var arc = g.selectAll(".arc")
-                            .data(pie(data))
+                            .data(pie(pie_data))
                             .enter().append("g")
                             .attr("class", "arc");
 
             arc.append("path")
                 .attr("d", path)
-                .attr("fill", function(d){ return color(d.data.name)})
-            });
+                .attr("fill", function(d){ return d.data.color});
             arc.append("text")
-                ..attr('transform', 'translate(' + label.centroid(d) + ')')
+                .attr('transform', 'translate(' + label.centroid(d) + ')')
                 .attr('dy', 'o.35em')
-                .text(function(d){ return d.data.funds})
-
+                .text(function(d){ return d.data.funds});
+        });
 }
